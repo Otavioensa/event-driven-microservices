f93f24a19f95123040041b29dac6d826f7eb4511
Otavioensa <otavioensa@gmail.com>
2020-11-23 19:44:58 +0100

    fix: open channel only once

--- api-service-application/src/libs/send-message-to-service.ts
+++ api-service-application/src/libs/send-message-to-service.ts
@@ -1,20 +1,22 @@
+/* tslint:disable no-console */
 import amqp from 'amqplib'
 import config from '../config'
 
 class SendMessageToService {
-  connection: amqp.Connection
+  protected connection: amqp.Connection
+  protected channel: amqp.ConfirmChannel
+
   constructor () {
     this.initialize()
   }
 
   async initialize () {
     this.connection = await amqp.connect(config.amqpUrl)
+    this.channel = await this.connection.createConfirmChannel()
   }
 
   async send (stringifiedPayload: string){
-    const channel = await this.connection.createConfirmChannel()
-    await channel.publish('processing', 'request', Buffer.from(stringifiedPayload, 'utf-8'))
-    await channel.close()
+    await this.channel.publish('processing', 'request', Buffer.from(stringifiedPayload, 'utf-8'))
   }
 }
 
